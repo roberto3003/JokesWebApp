679c1d4c21b98d1e0b61481181e0e7658c71c41b
bobby3003 <roberto3003@riseup.net>
2021-09-13 11:52:45 +0700

    database

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using JokesWebApp.Models;
 
 namespace JokesWebApp.Data;
 public class ApplicationDbContext : IdentityDbContext
@@ -8,4 +9,5 @@ public class ApplicationDbContext : IdentityDbContext
         : base(options)
     {
     }
+    public DbSet<JokesWebApp.Models.Joke> Joke { get; set; }
 }
